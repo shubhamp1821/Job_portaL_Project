d303ff8345f95794c43b158bd5742403c8e17ef2
shubhamp1821 <pagarshubham21@gmail.com>
2024-09-29 16:16:42 +0530

    second commit

--- Fronted/src/components/Layout/Footer.jsx
+++ Fronted/src/components/Layout/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   const { isAuthorized } = useContext(Context);
   return (
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
-      <div>&copy; All Rights Reserved By Pooja.</div>
+      <div>&copy; All Rights Reserved By Shubham.</div>
       <div>
         <Link to={""} target="_blank">
           <FaFacebookF />
